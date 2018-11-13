2d86f81ae526abb5c86e4a353d9dc663aa3ec22a
Di Stefano Luca <luca.distefano@intranet.previmedical.it>
2018-11-13 15:03:34 +0100

    successful test

--- server/src/test/scala/FailingTest.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest.{Matchers, WordSpec}
-
-class FailingTest extends WordSpec with Matchers {
-  "this test" should {
-    "fail" in {
-      fail()
-    }
-  }
-}
---
+++ server/src/test/scala/SuccessfulTest.scala
@@ -0,0 +1,9 @@
+import org.scalatest.{Matchers, WordSpec}
+
+class SuccessfulTest extends WordSpec with Matchers {
+  "this test" should {
+    "pass" in {
+
+    }
+  }
+}
