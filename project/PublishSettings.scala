38a4630e1db34fd20b9e7319da74c2403be57385
Di Stefano Luca <luca.distefano@intranet.previmedical.it>
2018-11-14 16:35:02 +0100

    test with sbt-release plugin

--- project/PublishSettings.scala
+++ project/PublishSettings.scala
@@ -1,8 +1,21 @@
 import bintray.BintrayKeys._
 import sbt.librarymanagement.LibraryManagementSyntax
+import sbtrelease.ReleasePlugin.autoImport._
+import ReleaseTransformations._
 
 object PublishSettings extends LibraryManagementSyntax {
   lazy val settings = Seq(
     bintrayOmitLicense := true,
+    releaseProcess := Seq[ReleaseStep](
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      releaseStepCommandAndRemaining("publish"),
+      setNextVersion,
+      commitNextVersion,
+      pushChanges
+    )
   )
+
+
 }
