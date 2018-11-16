da84107208e00feee3777f022914648574bd4946
Di Stefano Luca <luca.distefano@intranet.previmedical.it>
2018-11-16 14:42:25 +0100

    another git push test

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,5 +2,6 @@
 if [ $TRAVIS_BRANCH = "master" ]; then
     git checkout master
     sbt 'release with-defaults'
-    git push
+    git remote add origin-release https://${GITHUB_TOKEN}@github.com/LukeDS-it/test-heroku.git > /dev/null
+    git push --quiet --set-upstreaem origin-release master
 fi
